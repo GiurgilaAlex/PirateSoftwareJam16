72c7466c1b737a42d94b1bbc0015f662bdda808f
wjf37 <78282552+wjf37@users.noreply.github.com>
2025-01-25 16:30:35 +0000

    Updated camera zoom

--- PirateSoftwareJam/Assets/Scripts/CamFollowPlayer.cs
+++ PirateSoftwareJam/Assets/Scripts/CamFollowPlayer.cs
@@ -4,20 +4,26 @@ using UnityEngine;
 
 public class CamFollowPlayer : MonoBehaviour
 {
-    [SerializeField] GameObject player;
-    [SerializeField] Camera cam;
-    [SerializeField] float scrollSpeed;
-
+    [SerializeField] private GameObject player;
+    [SerializeField] private Camera cam;
+    [SerializeField] private float sensitivity;
+    [SerializeField] private float minZoom;
+    [SerializeField] private float maxZoom;
+    [SerializeField] private float speed;
+    private float targetZoom;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetZoom = cam.orthographicSize;
     }
 
     void Update()
     {
-        cam.orthographicSize += Input.mouseScrollDelta.y * scrollSpeed;
+        targetZoom -= Input.mouseScrollDelta.y * sensitivity;
+        targetZoom = Mathf.Clamp(targetZoom, minZoom, maxZoom);
+        float newSize = Mathf.MoveTowards(cam.orthographicSize, targetZoom, speed * Time.deltaTime);
+        cam.orthographicSize = newSize;
     }
     // Update is called once per frame
     void LateUpdate()
